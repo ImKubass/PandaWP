a103550495c595a7b3370155f24621232bc67bd4
Jakub Jetleb <jakubjetleb@gmail.com>
2019-06-26 15:27:20 +0200

    Quick text fixes

--- panda/Components/Footer/FooterBottom.php
+++ panda/Components/Footer/FooterBottom.php
@@ -1,11 +1,11 @@
 <div class="footer-bottom">
-	<div class="container">
-		<span class="footer-copy">Copyright &copy; <?= date("Y"); ?> - Všechna práva vyhrazena | Rolig.cz</span>
+    <div class="container">
+        <span class="footer-copy">Copyright &copy; <?= date("Y"); ?> - Všechna práva vyhrazena </span>
 
 
-		<span class="footer-scroll-top" data-kt-target="main">
-			<span><?php _e("Nahoru", "RLG_DOMAIN"); ?></span>
-			<img src="" data-src="<?= get_template_directory_uri(); ?>/images/ico/circle-arrow-bottom.svg" alt="">
-		</span>
-	</div>
+        <span class="footer-scroll-top" data-kt-target="main">
+            <span><?php _e("Nahoru", "RLG_DOMAIN"); ?></span>
+            <img src="" data-src="<?= get_template_directory_uri(); ?>/images/ico/circle-arrow-bottom.svg" alt="">
+        </span>
+    </div>
 </div>

254cd5895f8e7b741b53b4b1188b480c5fcd8678
Jakub Jetleb <jakubjetleb@gmail.com>
2020-01-13 12:51:33 +0100

    Posts and Products Query classes -  basic

--- panda/Components/PostsQuery/PostsQuery.php
+++ panda/Components/PostsQuery/PostsQuery.php
@@ -12,30 +12,4 @@ class PostsQuery extends QueryBase
     {
         parent::__construct();
     }
-
-    private function initPosts()
-    {
-
-        $args = [
-            "post_type" => KT_WP_POST_KEY,
-            "post_status" => "publish",
-            "posts_per_page" => $this->getMaxCount(),
-            "orderby" => "date",
-            "order" => \KT_Repository::ORDER_DESC,
-        ];
-        // except himself
-        if (is_single()) {
-            $args["post__not_in"] = [get_the_ID()];
-        }
-
-        $query = new \WP_Query();
-        $posts = $query->query($args);
-        if (Util::arrayIssetAndNotEmpty($posts)) {
-            $this->posts = $posts;
-            $this->postsCount = count($posts);
-        } else {
-            $this->posts = [];
-            $this->postsCount = 0;
-        }
-    }
 }
--- panda/Components/ProductsQuery/ProductsQuery.php
+++ panda/Components/ProductsQuery/ProductsQuery.php
@@ -11,5 +11,22 @@ class ProductsQuery extends QueryBase
     {
         parent::__construct();
         $this->setPostType(PRODUCT_KEY);
+        $this->setComponentLoopName("Item");
+        $this->initArgs();
+    }
+
+
+    // Custom Args for Query
+    public function initArgs()
+    {
+        $Args = [
+            "post_type" => $this->getPostType(),
+            "post_status" => "publish",
+            "posts_per_page" => $this->getMaxCount(),
+            "orderby" => "date",
+            "order" => \KT_Repository::ORDER_DESC,
+        ];
+
+        return $this->setArgs($Args);
     }
 }
